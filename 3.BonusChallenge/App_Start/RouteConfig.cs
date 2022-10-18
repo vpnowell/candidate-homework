9b7da79512d00d946d485357c8a3f7487e415478
vpnowell <vincentpaulnowell@gmail.com>
2022-10-18 09:21:41 -0700

    modified RouteConfig.cs --removed unnecessary routes

--- 3.BonusChallenge/App_Start/RouteConfig.cs
+++ 3.BonusChallenge/App_Start/RouteConfig.cs
@@ -12,12 +12,6 @@ namespace _3.BonusChallenge_1
             routes.MapRoute("Index", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
 
-            routes.MapRoute("List1", "{controller}/{action}/{id}", new { controller = "Home", action = "List1", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute("List2", "{controller}/{action}/{id}", new { controller = "Home", action = "List2", id = UrlParameter.Optional }
-            );
-
             routes.MapRoute("Results", "{controller}/{action}/{id}", new { controller = "Home", action = "Results", id = UrlParameter.Optional }
             );
 
